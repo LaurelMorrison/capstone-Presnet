ec7159127ee97baf70a6052ac29462d6da1cd942
Laurel Morrison <lbentleym1992@gmail.com>
2021-08-03 16:20:55 -0400

    adding status

--- Presnet/Program.cs
+++ Presnet/Program.cs
@@ -6,14 +6,33 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
 
 namespace Presnet
 {
     public class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
+
             CreateHostBuilder(args).Build().Run();
+
+            //// Find your Account SID and Auth Token at twilio.com/console
+            //// and set the environment variables. See http://twil.io/secure
+            //string accountSid = Environment.GetEnvironmentVariable("AC65d431a916889892439cad0ac7fa11d7");
+            //string authToken = Environment.GetEnvironmentVariable("2804d5191475585dae4738ba7ae357cc");
+
+            //TwilioClient.Init(accountSid, authToken);
+
+            //var message = MessageResource.Create(
+            //    body: "Join Earth's mightiest heroes. Like Kevin Bacon.",
+            //    from: new Twilio.Types.PhoneNumber("+12673100869"),
+            //    to: new Twilio.Types.PhoneNumber("+18045485318")
+            //);
+
+            //Console.WriteLine(message.Sid);
+
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
